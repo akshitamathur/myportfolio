bbdbaff73eb1af54adab7bdc4765742f544c45e5
Akshita <mathur_akshita@yahoo.ca>
2024-06-11 14:36:26 -0400

    Title Update feature complete
    - missing animations but updates lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
 import Data from "./assets/json/resume.json";
 import "./assets/css/App.css";
 import Root from "./routes/root";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import Socials from "./components/socials";
 
+
 function App() {
-  // const [count, setCount] = useState(0);
+
+  const [currentTitle, setCurrentTitle] = useState(0);
   const dataContent = Data.basics;
   const jobTitles = Data.basics.titles;
 
-  const element = jobTitles
-    .sort(() => Math.random() - Math.random())
-    .find(() => true);
+  // const element = jobTitles
+  //   .sort(() => Math.random() - Math.random())
+  //   .find(() => true);
+
+  // const titles=["Sharepoint", "Web", "FrontEnd", "Shopify", "vue", "React"];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const num = Math.floor(Math.random() * jobTitles.length);
+            setCurrentTitle(num);
+        }, 4000)
+        
+        return () => clearInterval(intervalId);
+    }, [])
+
+   
+ 
 
   return (
     <>
@@ -39,7 +55,8 @@ function App() {
           >
             <h1 className="h1Heading mb-5">{dataContent.name.toUpperCase()}</h1>
             <h3 className="title mb-5">
-              <span className="orange">{element?.name} </span>
+              {/* <span className="orange">{(element?.name)} </span> */}
+              {<span key={Math.random()} className="orange animate__animated animate__fadeIn animate__delay-3s">{jobTitles[currentTitle].name} </span>}
               Developer
             </h3>
             <p className="mb-5">{dataContent.summary}</p>
